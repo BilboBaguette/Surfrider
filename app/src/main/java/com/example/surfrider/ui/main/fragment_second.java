c2686a5eeb0c761907c0a9dbf6a38d005182fb9c
BilboBaguette <roman@didelet.fr>
2020-12-04 13:09:04 +0900

    Séparation des fragments

---
+++ app/src/main/java/com/example/surfrider/ui/main/fragment_second.java
@@ -0,0 +1,16 @@
+package com.example.surfrider.ui.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.surfrider.R;
+
+public class fragment_second extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+}
